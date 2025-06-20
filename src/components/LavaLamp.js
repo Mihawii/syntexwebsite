1a99041a04f345db2b4b0463ef72e9ec6908f02d
Olzhas Mukhtarov <mihawii@Olzhass-MacBook-Air.local>
2025-06-21 00:09:38 +0500

    Make LavaLamp visible: transparent App background & shader bg black

--- src/components/LavaLamp.js
+++ src/components/LavaLamp.js
@@ -104,7 +104,7 @@ const fragmentShader = `
           color = vec3(fresnel);
           gl_FragColor = vec4(color, 1.0);
       } else {
-          gl_FragColor = vec4(1.0);
+          gl_FragColor = vec4(0.0);
       }
   }
 `;
